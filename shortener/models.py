7b710bcab967f497323224a37138dbdae1ee0f59
Balaji Selvam <balajiselvam005@gmail.com>
2026-03-31 00:35:48 +0530

    basic url shortening feature

--- shortener/models.py
+++ shortener/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+import string, secrets
 
-# Create your models here.
+def generate_code():
+    chars = string.ascii_letters + string.digits
+    while True:
+        code = ''.join(secrets.choice(chars) for _ in range(6))
+        if not ShortURL.objects.filter(short_code=code).exists():
+            return code
+
+class ShortURL(models.Model):
+    original_url = models.URLField()
+    short_code = models.CharField(max_length=6, unique=True, db_index=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    click_count = models.PositiveIntegerField(default=0)
+
+    def save(self, *args, **kwargs):
+        if not self.short_code:
+            self.short_code = generate_code()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.short_code
--- shortener/views.py
+++ shortener/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import ShortURL
 
-# Create your views here.
+def home(request):
+    short_url = None
+    if request.method == "POST":
+        original_url = request.POST.get("url")
+        obj = ShortURL.objects.create(original_url=original_url)
+        short_url = request.build_absolute_uri(f"/s/{obj.short_code}/")
+    return render(request, "home.html", {"short_url": short_url})
